74ca4bf2e6bee2c6f235b305d84c22035b45f4d3
ezequiaslopesdasilva <wyskzito@gmail.com>
2024-12-15 13:19:44 -0300

    feat: add unique ID, equals method, and toString implementation to Key class

--- src/core/key.ts
+++ src/core/key.ts
@@ -6,9 +6,13 @@ type KeyProps = (TKey & Partial<TCode>) | (TCode & Partial<TKey>);
 export class Key {
   public key: string;
   public code: string;
+  public readonly id: string;
   private onPressCallback?: (event: KeyboardEvent, key: Key) => void;
   private onReleaseCallback?: (event: KeyboardEvent, key: Key) => void;
+  private onLongPressCallback?: (event: KeyboardEvent, key: Key) => void;
   private isPressed: boolean = false;
+  private longPressTimer: NodeJS.Timeout | null = null;
+  private longPressDuration: number = 500;
   private stateChangeListener?: (state: boolean) => void;
   private previousState: boolean = false;
 
@@ -16,6 +20,7 @@ export class Key {
     this.key = key;
     this.code = code;
     this.initListeners();
+    this.id = `${key || code}:${code || key}`;
     this.previousState = this.getState();
   }
 
@@ -34,6 +39,9 @@ export class Key {
       !this.isPressed
     ) {
       this.isPressed = true;
+      this.longPressTimer = setTimeout(() => {
+        this.onLongPressCallback?.(event, this);
+      }, this.longPressDuration);
       this.onPressCallback?.(event, this);
     }
   };
@@ -41,6 +49,10 @@ export class Key {
   private handleKeyUp = (event: KeyboardEvent) => {
     if (event.code === this.code || event.key === this.key) {
       this.isPressed = false;
+      if (this.longPressTimer) {
+        clearTimeout(this.longPressTimer);
+        this.longPressTimer = null;
+      }
       this.onReleaseCallback?.(event, this);
     }
   };
@@ -64,6 +76,18 @@ export class Key {
     this.initListeners();
   }
 
+  toString(): string {
+    return `Key(${this.key}, ${this.code})`;
+  }
+
+  static isEquals(key: Key, other: Key): boolean {
+    return key.id === other.id;
+  }
+
+  equals(other: Key): boolean {
+    return this.id === other.id;
+  }
+
   onPress(callback: (event: KeyboardEvent, key: Key) => void) {
     this.onPressCallback = callback;
   }
@@ -72,13 +96,20 @@ export class Key {
     this.onReleaseCallback = callback;
   }
 
+  onLongPress(
+    callback: (event: KeyboardEvent, key: Key) => void,
+    duration: number = 500
+  ) {
+    this.onLongPressCallback = callback;
+    this.longPressDuration = duration;
+  }
+
   isKeyPressed(): boolean {
     return this.isPressed;
   }
 
   getState(): boolean {
     const event = new KeyboardEvent("keydown");
-
     return event.getModifierState(this.key);
   }
 
